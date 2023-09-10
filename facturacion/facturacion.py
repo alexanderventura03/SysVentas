788a3c1dd33e12fa7f8f5eda17c994644bc36fcc
Alexander200330 <alexanderventura0630@gmail.com>
2023-09-10 18:52:26 -0400

    Configurar ventana de la aplicacion

---
+++ facturacion/facturacion.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Inicializar la aplicación Tkinter
+aplicacion = Tk()
+aplicacion.geometry('1000x610+0+0')
+aplicacion.title("SysVentas")
+aplicacion.config(bg="burlywood")
+aplicacion.resizable(0,0);
+
+# Evitar que la aplicación se cierre
+aplicacion.mainloop()
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from forms.form_login import App
+from facturacion.facturacion import aplicacion
 
-App()
+aplicacion
